d9c930ccf308156e3229b69563225fcde5c3898f
vamsi krishna <gvamsikrishna71817@gmail.com>
2024-08-23 19:47:26 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'screens/admin_login_page.dart';
+import 'screens/location_selection_page.dart';
+import 'screens/welcome_page.dart'; 
+
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //title: 'Water Sanitation App',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      initialRoute: '/location_selection',
+      routes: {
+        '/location_selection': (context) => LocationSelectionPage(),
+       // '/admin_login': (context) => AdminLoginPage(),
+       //'/welcome':(context) => WelcomePage(),
+      },
+    );
+  }
+}
